f18c079a0e5992bffdaafbdced54c25adf924f63
Ramos <jhonatanramos944@gmail.com>
2026-05-19 00:59:52 -0500

    Actualiza UserSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -10,31 +10,40 @@ class UserSeeder extends Seeder
 {
     public function run(): void
     {
-        // Crear el usuario admin
-        $admin = User::create([
-            'name' => 'Admin',
-            'email' => 'ayniforestperu@gmail.com',
-            'email_verified_at' => now(),
-            'password' => Hash::make('Ayni_2026-Fores7'),
-        ]);
+        // Admin
+        $admin = User::updateOrCreate(
+            ['email' => 'ayniforestperu@gmail.com'],
+            [
+                'name' => 'Admin',
+                'email_verified_at' => now(),
+                'password' => Hash::make('Ayni_2026-Fores7'),
+            ]
+        );
+
         $admin->assignRole('Admin');
 
-        // Crear un usuario con rol Vendedor
-        $vendedor = User::create([
-            'name' => 'vendedor',
-            'email' => 'Aynivendedor@gmail.com',
-            'email_verified_at' => now(),
-            'password' => Hash::make('20.Vendedor.26'),
-        ]);
+        // Vendedor
+        $vendedor = User::updateOrCreate(
+            ['email' => 'Aynivendedor@gmail.com'],
+            [
+                'name' => 'vendedor',
+                'email_verified_at' => now(),
+                'password' => Hash::make('20.Vendedor.26'),
+            ]
+        );
+
         $vendedor->assignRole('Vendedor');
 
-        // Crear un usuario con rol Usuario
-        $usuario = User::create([
-            'name' => 'usuario',
-            'email' => 'usuario@gmail.com',
-            'email_verified_at' => now(),
-            'password' => Hash::make('usuario123'),
-        ]);
+        // Usuario
+        $usuario = User::updateOrCreate(
+            ['email' => 'usuario@gmail.com'],
+            [
+                'name' => 'usuario',
+                'email_verified_at' => now(),
+                'password' => Hash::make('Usuario153'),
+            ]
+        );
+
         $usuario->assignRole('Usuario');
     }
 }
